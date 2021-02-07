233f5dc7b6b50076f22ee459f003a4150596b095
Skeleton <chaos0824@naver.com>
2021-02-07 21:52:36 +0900

    테스트용 Grid 스크립트, 관리 스크립트 구현

---
+++ Assets/Scripts/GridBuildingSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBuildingSystem : MonoBehaviour
+{
+    [SerializeField] private int Width = 0;
+    [SerializeField] private int Height = 0;
+    [SerializeField] private float CellSize = 0f;
+    [SerializeField] private Transform centerTransform = null;
+
+    private GridXZ<GridObject> grid = null;
+
+    private void Awake()
+    {
+        grid = new GridXZ<GridObject>(centerTransform, Width, Height, CellSize, centerTransform.position, 
+                                      (GridXZ<GridObject> g, int x, int z) => new GridObject(g, x, z));
+    }
+
+    public class GridObject
+    {
+        private GridXZ<GridObject> grid;
+        private int x;
+        private int z;
+
+        public GridObject(GridXZ<GridObject> grid, int x, int z)
+        {
+            this.grid = grid;
+            this.x = x;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}", x, z);
+        }
+    }
+}
---
+++ Assets/Scripts/GridXZ.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+public class GridXZ<GridObject>
+{
+    public event EventHandler<OnGridValueChangedEventArgs> OnValueChangedEvent = null;
+    public class OnGridValueChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int z;
+    }
+
+    private int width = 0;
+    private int height = 0;
+    private float cellSize = 0;
+    private Vector3 center = Vector3.zero;
+    private Transform textParent = null;
+    private GridObject[,] gridArray = null;
+
+    public GridXZ(Transform parent, int width, int height, float cellSize, Vector3 center, Func<GridXZ<GridObject>, int, int, GridObject> createGridObject)
+    {
+        this.width = width;
+        this.height = height;
+        this.center = center;
+        this.cellSize = cellSize;
+        this.textParent = parent;
+
+        gridArray = new GridObject[width, height];
+
+        Vector3 worldPosition;
+        Vector3 textPlusPosition = new Vector3(cellSize / 2, 0, cellSize / 2);
+        var debugTextArray = new TextMesh[width, height];
+        for(int x = 0; x < width; x++)
+        {
+            for(int z =0; z < height; z++)
+            {
+                gridArray[x, z] = createGridObject(this, x, z);
+                worldPosition = GetWorldPosition(x, z);
+                debugTextArray[x, z] = CreateText(worldPosition + textPlusPosition, gridArray[x, z].ToString(), Color.white, TextAnchor.MiddleCenter, 20, TextAlignment.Center);
+                Debug.DrawLine(worldPosition, GetWorldPosition(x, z + 1), Color.white, 100f);
+                Debug.DrawLine(worldPosition, GetWorldPosition(x + 1, z), Color.white, 100f);
+            }
+        }
+        worldPosition = GetWorldPosition(width, height);
+        Debug.DrawLine(GetWorldPosition(0, height), worldPosition, Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), worldPosition, Color.white, 100f);
+
+        OnValueChangedEvent += (object sender, OnGridValueChangedEventArgs eventArgs) =>
+        { debugTextArray[eventArgs.x, eventArgs.z].text = gridArray[eventArgs.x, eventArgs.z].ToString(); };
+    }
+
+    private TextMesh CreateText(Vector3 position, string text, Color color, TextAnchor anchor, int fontSize, TextAlignment alignment)
+    {
+        var textObject = new GameObject("World_Text", typeof(TextMesh));
+        textObject.transform.position = position;
+        textObject.transform.SetParent(textParent);
+        var textMesh = textObject.GetComponent<TextMesh>();
+        textMesh.text = text;
+        textMesh.color = color;
+        textMesh.anchor = anchor;
+        textMesh.fontSize = fontSize;
+        textMesh.alignment = alignment;
+        return textMesh;
+    }
+
+    private Vector3 GetWorldPosition(int x, int z)
+    {
+        return new Vector3(x, 0, z) * cellSize + center;
+    }
+}

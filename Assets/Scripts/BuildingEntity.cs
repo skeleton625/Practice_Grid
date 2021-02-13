085ee0df524c42b1bfb264d9d8b2b873d82b9236
Skeleton <chaos0824@naver.com>
2021-02-13 21:39:34 +0900

    건물 정보 관리 스크립트 구현

---
+++ Assets/Scripts/BuildingEntity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingEntity : MonoBehaviour
+{
+    public static BuildingEntity Create(Vector3 worldPosition, Vector2Int origin , Dir dir, BuildingData buildData)
+    {
+        var buildTransform = Instantiate(buildData.prefab, worldPosition, Quaternion.Euler(0, BuildingData.GetRotationAngle(dir), 0));
+        var buildingEntity = buildTransform.GetComponent<BuildingEntity>();
+
+        buildingEntity.buildData = buildData;
+        buildingEntity.origin = origin;
+        buildingEntity.dir = dir;
+
+        return buildingEntity;
+    }
+
+    public List<Vector2Int> GetGridPositionList()
+    {
+        return buildData.GetGridPositinoList(origin, dir);
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    private BuildingData buildData = null;
+    private Vector2Int origin = default;
+    private Dir dir = default;
+}

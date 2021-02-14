342ecf0d6f82981dd1862a90b7474f24501d2ca5
Skeleton <chaos0824@naver.com>
2021-02-14 22:04:25 +0900

    RaycastCamera 함수 추가

---
+++ Assets/Scripts/UtilClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilClass
+{
+    private static Camera mainCamera = null;
+
+    public static void InitializeUtilClass()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public static Vector3 RaycastCamera()
+    {
+        var cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(cameraRay.origin, cameraRay.direction, out RaycastHit hit, 1000f))
+            return hit.point;
+        return Vector3.zero;
+    }
+}

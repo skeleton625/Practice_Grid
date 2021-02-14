6cf8c24ada3ad465940404859d9c4d1c22f3ac8e
Skeleton <chaos0824@naver.com>
2021-02-14 22:05:14 +0900

    설치 건물 선택 시, 예시 위치, 회전, 모습을 보여주는 스크립트 추가

---
+++ Assets/Scripts/BuildingModelController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public class BuildingModelController : MonoBehaviour
+{
+    [SerializeField] private float LerpScale = 0;
+
+    private Transform visual = null;
+    private GridBuildingSystem buildingSystem = null;
+
+    private void Start()
+    {
+        buildingSystem = GridBuildingSystem.Instance;
+        buildingSystem.OnSelectedChanged += Event_OnSlectedChanged;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = buildingSystem.GetMouseGridSnappedPosition() + Vector3.up;
+        Quaternion targetRotation = buildingSystem.GetBulidingEntityRotation();
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * LerpScale);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * LerpScale);
+    }
+
+    private void RefreshVisual()
+    {
+        if(visual != null)
+        {
+            Destroy(visual.gameObject);
+            visual = null;
+        }
+
+        var preBuildingData = buildingSystem.GetSelectedBuildingData();
+        if(preBuildingData != null)
+        {
+            visual = Instantiate(preBuildingData.visual, transform);
+            visual.localPosition = Vector3.zero;
+            visual.localEulerAngles = Vector3.zero;
+            SetLayerRecursive(visual.gameObject, 9);
+        }
+    }
+
+    private void SetLayerRecursive(GameObject targetParent, int layer)
+    {
+        targetParent.layer = layer;
+        foreach (Transform targetChild in targetParent.transform)
+            SetLayerRecursive(targetChild.gameObject, layer);
+    }
+
+    private void Event_OnSlectedChanged(object sender, System.EventArgs e)
+    {
+        RefreshVisual();
+    }
+}
